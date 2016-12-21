b17085441f8e21149707ca6caef91650119f754e
mint-cipher <cpumaster930@optonline.net>
2016-12-21 12:11:53 -0500

    more features
    auto resize

--- script.js
+++ script.js
@@ -21,14 +21,13 @@ function makeEmoteText() {
     var w = $("#gridWidth").val();
     var onEmote = $("#emoteOn").val();
     var offEmote = $("#emoteOff").val();
-    for (var i = 0; i < h; i++) {
-        for (var j = 0; j < w; j++) {
+    for (var i = 0; i < Math.min(GRID_HEIGHT, h); i++) {
+        for (var j = 0; j < Math.min(GRID_WIDTH, w); j++) {
             var isOn = $("#box" + i + "-" + j).prop("checked");
             console.log(i, j, isOn);
             if (isOn) {
                 out += onEmote;
-            }
-            else {
+            } else {
                 out += offEmote;
             }
         }
@@ -36,4 +35,22 @@ function makeEmoteText() {
     }
     $("#results").empty();
     $("#results").append(out);
+}
+
+function autoResize() {
+    var newWidth = 0;
+    var newHeight = 0;
+    for (var i = 0; i < GRID_HEIGHT; i++) {
+        for (var j = 0; j < GRID_WIDTH; j++) {
+            var isOn = $("#box" + i + "-" + j).prop("checked");
+            if (isOn && i + 1 > newHeight) {
+                newHeight = i + 1;
+            }
+            if (isOn && j + 1 > newWidth) {
+                newWidth = j + 1;
+            }
+        }
+    }
+    $("#gridHeight").val(newHeight);
+    $("#gridWidth").val(newWidth);
 }
